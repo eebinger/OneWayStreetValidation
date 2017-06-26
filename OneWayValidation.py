90c5b12cb65bad3254996033f452c2bc53bd3cc3
Ethan Ebinger <eebinger@ctps.org>
2017-06-26 11:50:03 -0400

    Update OneWayValidation.py
    Additional qualifiers added to ensure no routes that need to be flipped are skipped over due to improper snapping by Google Maps Roads API. Heuristics test the distance between sent and snapped points (see lines 277-281) and the orientation of sent and snapped lines (see lines 283-290), and were empirically derived.
    Parameter to input Densify distance added. Parameter to save (lat,lon) CSV from feature class updated so it is properly saved.
    Flip Lines default value now 'yes'.

--- OneWayValidation.py
+++ OneWayValidation.py
@@ -24,6 +24,7 @@ import arcpy
 import csv
 import json
 import urllib2
+import math
 
 # The road network to validate. Must be a polyline feature class.
 # Input is used only for road segment selection, geoprocessing is not done on this feature class.
@@ -46,12 +47,20 @@ unique_id = arcpy.GetParameterAsText(3)
 # This parameter is optional. Leave blank if no selection by attribute is desired.
 street_select_expression = arcpy.GetParameterAsText(4)
 
+# Distance (in meters) at which to Densify polyline segments.
+# It is recommended to use in combination with the SQL Expression, and call
+# segments based on length:
+#	if Length >= 30: densify_distance = 10 Meters
+#	if Length < 30 and Length >= 10: densify_distance = 5 Meters
+# 	if Length < 10: densify_distance = 1 Meter
+densify_distance = arcpy.GetParameterAsText(5)
+
 # File name for the output of Step 4:
-fc_Densify_VertPoints = arcpy.GetParameterAsText(5)
+fc_Densify_VertPoints = arcpy.GetParameterAsText(6)
 fc_Densify_VertPoints = working_gdb + '\\' + working_fc + '_Densify_VertPoints'
 
 # File name for the output of Step 5:
-fc_Densify_VertPoints_Project = arcpy.GetParameterAsText(6)
+fc_Densify_VertPoints_Project = arcpy.GetParameterAsText(7)
 fc_Densify_VertPoints_Project = fc_Densify_VertPoints + '_WGS84'
 
 # The names of the fields to be exported to a CSV (saved as a .txt file). 
@@ -61,13 +70,9 @@ fc_Densify_VertPoints_Project = fc_Densify_VertPoints + '_WGS84'
 #		Unique identifier (i.e. ROADINVENTORY_ID)
 #		POINT_X
 #		POINT_Y
-Value_Field = arcpy.GetParameterAsText(7)
+Value_Field = arcpy.GetParameterAsText(8)
 if Value_Field == '#' or not Value_Field:
-    Value_Field = unique_id + ";ORIG_FID;POINT_X;POINT_Y"
-
-# File name for the TXT output, from (7):
-csv_output = arcpy.GetParameterAsText(8)
-csv_output = fc_Densify_VertPoints_Project + '_addXY.txt'
+    Value_Field = unique_id + ";POINT_X;POINT_Y"
 
 # Your Google Maps API Key. Should be a 39 character string acquired through 
 # creation of credentials via the Google API Manager: 
@@ -83,18 +88,38 @@ key = arcpy.GetParameterAsText(9)
 # in the wrong direction of a one-way street will be edited and the start and end points 
 # will be flipped (using FlipLines_Edit: http://desktop.arcgis.com/en/arcmap/10.3/tools/editing-toolbox/flip-line.htm) 
 # in the Working Feature Class.
-# Else, leave field as 'No' to skip processes of flipping polylines for now.
+# Else, change field to 'No' to skip processes of flipping polylines for now.
 flip_routes = arcpy.GetParameterAsText(10)
 if flip_routes == '#' or not flip_routes:
-    flip_routes = 'No'
+    flip_routes = 'Yes'
 
 # The pathname of the folder where CSVs generated from this script, 
 # including a .txt file with the unique ids of road segments to flip, will be saved.
 flip_ids_output = arcpy.GetParameterAsText(11)
 
+# File name for the TXT output, from (7):
+csv_output = arcpy.GetParameterAsText(12)
+csv_output = fc_Densify_VertPoints_Project + '_addXY.txt'
+
 # File name for the output of Step 11:
-flip_ids_txt = arcpy.GetParameterAsText(12)
+flip_ids_txt = arcpy.GetParameterAsText(13)
 flip_ids_txt = flip_ids_output + '\\' + working_fc + "_flip_ids.txt"
+
+# Function from https://gist.github.com/jeromer/2005586 that is used to calculate
+# the orientation of the sent and returned lines to verify if it was snapped to the
+# correct road:
+def calculate_initial_compass_bearing(pointA, pointB):
+	if (type(pointA) != tuple) or (type(pointB) != tuple):
+		raise TypeError("Only tuples are supported as arguments")
+	lat1 = math.radians(pointA[0])
+	lat2 = math.radians(pointB[0])
+	diffLong = math.radians(pointB[1] - pointA[1])
+	x = math.sin(diffLong) * math.cos(lat2)
+	y = math.cos(lat1) * math.sin(lat2) - (math.sin(lat1) * math.cos(lat2) * math.cos(diffLong))
+	initial_bearing = math.atan2(x, y)
+	initial_bearing = math.degrees(initial_bearing)
+	compass_bearing = (initial_bearing + 360) % 360
+	return compass_bearing
 
 # ---------------------------------------------------------------------------
 
@@ -117,9 +142,9 @@ arcpy.MakeFeatureLayer_management(
 arcpy.Densify_edit(
 	in_features = 'road_seg_working', 
 	densification_method = "DISTANCE", 
-	distance = "10 Meters"
+	distance = densify_distance
 )
-arcpy.AddMessage( "Polylines densified by distance every 10 meters.")
+arcpy.AddMessage( "Polylines densified by distance every " + str(densify_distance))
 
 # 4) Feature Vertices To Points
 arcpy.FeatureVerticesToPoints_management(
@@ -174,7 +199,7 @@ with open (csv_output, 'rb') as infile:
 			id = id_val
 			prev_id = id_val
 			roadInvIDs.append(id_val)
-			arcpy.AddMessage("now saving (lat,lng) from id segment " + str(id_val))
+			#arcpy.AddMessage("now saving (lat,lng) from id segment " + str(id_val))
 			snap_list_num += 1
 			snap_list[snap_list_num]['latlng'].append([row['POINT_Y'],row['POINT_X']])
 		elif id == id_val and id_val != unique_id:
@@ -200,43 +225,84 @@ arcpy.AddMessage("(lat,lng) pairs, Snap To Roads url request collected for each 
 	
 # 10) For each unique ID, call Snap To Roads via Google Maps Roads API. If the 
 #	  number of points returned != the number of points sent, then the road 
-#	  segment needs to be flipped and the id is saved in "flip_ids" list
+#	  segment needs to be flipped and the id is saved in "flip_ids" list.
+#	  Some records are skipped because they are either too short or are not present
+#	  in Google Maps, so those are saved in the "skipped_ids" list.
+#	  Other road segments that may need to be flipped but were still returned as
+#	  snapped by the Snap To Roads service are saved in the "potential_flip_ids" list
+#	  and should be manually reviewed in ArcGIS by the user.
 flip_ids = []
+potential_flip_ids = []
+skipped_ids = []
+
 if len(snap_list) > 2500:
 	arcpy.AddMessage("List is greater than 2500 points. Only the first 2500 values in 'snap_list' were queried.")
 for i in snap_list[:2499]:
-	arcpy.AddMessage(i['id'])
+	#arcpy.AddMessage(i['id'])
 	if len(i['latlng'][1:-1]) <= 1:
-		arcpy.AddMessage("id " + str(i['id']) + " skipped because too few points (<=1)")
+		#arcpy.AddMessage("id " + str(i['id']) + " skipped because too few points (<=1)")
+		skipped_ids.append(i['id'])
 		continue
-	elif len(i['latlng']) > 102:
-		arcpy.AddMessage("# of points sent = 100")
-	else:
-		arcpy.AddMessage("# of points sent = " + str(len(i['latlng'][1:-1])))
+	#elif len(i['latlng']) > 102:
+		#arcpy.AddMessage("# of points sent = 100")
+	#else:
+		#arcpy.AddMessage("# of points sent = " + str(len(i['latlng'][1:-1])))
 	handle = urllib2.urlopen(i['url'])
 	url_return = handle.read()
 	snapped_points = json.loads(url_return)
 	if len(snapped_points) != 1:
-		arcpy.AddMessage("id " + str(i['id']) + " skipped because segment not in Google Maps")
+		#arcpy.AddMessage("id " + str(i['id']) + " skipped because segment not in Google Maps")
+		skipped_ids.append(i['id'])
 		continue
 	else:
 		i['snapped_points'] = snapped_points['snappedPoints']
-		arcpy.AddMessage("# of points recieved = " + str(len(i['snapped_points'])))
+		#arcpy.AddMessage("# of points recieved = " + str(len(i['snapped_points'])))
 		if len(i['latlng']) > 102:
 			if len(i['snapped_points']) != 100:
 				flip_ids.append(i['id'])
 		else:
 			if len(i['latlng'][1:-1]) != len(i['snapped_points']):
 				flip_ids.append(i['id'])
+			else: 
+				lat_start = float(i['latlng'][1][0])
+				lat_end = float(i['latlng'][-2][0])
+				lng_start = float(i['latlng'][1][1])
+				lng_end = float(i['latlng'][-2][1])
+				
+				snap_lat_start = i['snapped_points'][0]['location']['latitude']
+				snap_lat_end = i['snapped_points'][-1]['location']['latitude']
+				snap_lng_start = i['snapped_points'][0]['location']['longitude']
+				snap_lng_end = i['snapped_points'][-1]['location']['longitude']
+				
+				orig_distance = math.sqrt(math.pow(lat_end - lat_start,2)+math.pow(lng_end - lng_start,2))
+				snap_distance = math.sqrt(math.pow(snap_lat_end - lat_start,2)+math.pow(snap_lng_end - lng_start,2))
+				if (abs(snap_distance - orig_distance)*100000) > 4:		# empirically derived
+					#arcpy.AddMessage("manually check id " + str(i['id']) + " b/c snap distances off")
+					potential_flip_ids.append(i['id'])
+								
+				dir_orig = calculate_initial_compass_bearing((lat_start, lng_start),(lat_end, lng_end))
+				dir_snap = calculate_initial_compass_bearing((snap_lat_start, snap_lng_start),(snap_lat_end, snap_lng_end))
+				if (abs(dir_orig - dir_snap)) > 2:		# empirically derived
+					#arcpy.AddMessage("manually check id " + str(i['id']) + " b/c snap orientation off")
+					if i['id'] in potential_flip_ids:
+						flip_ids.append(i['id'])
+					else:
+						potential_flip_ids.append(i['id'])
 
+arcpy.AddMessage("IDs to flip: ")
+arcpy.AddMessage(flip_ids)
+arcpy.AddMessage("IDs to check manually and potentially flip: ")
+arcpy.AddMessage(potential_flip_ids)
+arcpy.AddMessage("IDs that were skipped: ")
+arcpy.AddMessage(skipped_ids)
 arcpy.AddMessage("Snap To Roads called for each unique ID, segments to be flipped collected.")
 
 # 11) Export list of ids to flip (aka "flip_ids") and the whole "snap_list" to CSV
 with open (flip_ids_txt, 'wb') as f:
 	w = csv.writer(f, delimiter=',')
-	w.writerow(flip_ids)	
-with open (flip_ids_txt, 'a') as f:
-	w = csv.writer(f, delimiter=',')
+	w.writerow(flip_ids)
+	w.writerow(potential_flip_ids)
+	w.writerow(skipped_ids)
 	w.writerow(snap_list)
 arcpy.AddMessage("Wrongly digitized polyline segments to saved: " + flip_ids_txt)
 		
